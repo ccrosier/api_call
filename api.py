9e50961b10c1d6512c7a6d17811447c69b9760d0
Ryan Crosier <crosier.17@osu.edu>
2024-08-22 14:31:09 -0400

    added pokemon input field

--- api.py
+++ api.py
@@ -1,6 +1,12 @@
 import requests
 import pprint
-url = "https://pokeapi.co/api/v2/pokemon/ditto"
-response = requests.get(url)
-pprint.pprint(response.json())
+
+pokemon = input("Enter name of pokemon: ")
+url = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
+
+try:
+    response = requests.get(url)
+    pprint.pprint(response.json(), compact=True)
+except:
+    print(f"Error making API call to: {url}\n\t{response}")
 
